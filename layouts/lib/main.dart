1cbd921a5ed4da6cebc267c9b1b78efb23797a77
procs <procs@main.lan>
2017-10-15 01:47:58 +0200

    layout tutorial self rebuild -> my step 01

--- layouts/lib/main.dart
+++ layouts/lib/main.dart
@@ -45,67 +45,101 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  //int _counter = 0;
 
-  void _incrementCounter() {
+  void _nothingToSeeHere() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      //_counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+    return new Column(
+      children: <Widget>[
+        new Image.network(  
+            'https://i.ytimg.com/vi/WFlrSZcWwIk/maxresdefault.jpg',
+            repeat: ImageRepeat.noRepeat, // repeat does not seem to work
+            scale: 3.0, // scale does not seem to work
             ),
-            new Text(
-              '${_counter}',
-              style: Theme.of(context).textTheme.display1,
+        new Row(
+          children: <Widget>[
+            new Expanded(
+              child:
+            new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text("First Text",
+                  style: Theme.of(context).primaryTextTheme.title,
+                  textAlign: TextAlign.left,
+                  ),
+                new Text("Second Text",
+                  style: Theme.of(context).primaryTextTheme.title,
+                  ),
+              ],
+            ),
+            ),
+            new Icon(Icons.star,
+              color: new Color(0xFFFF0000),
+              ),
+            new Text("Counter",
+              style: Theme.of(context).primaryTextTheme.title,
+              ),
+          ],
+        ),
+        new Row(
+          children: <Widget>[
+            new Expanded(child:
+            new Column(
+              children: <Widget>[
+                new Icon(Icons.phone,
+                  color: new Color(0xFF0000FF),
+                  ),
+                new Text("Tile 1",
+                  style: Theme.of(context).primaryTextTheme.title,
+                  ),
+              ],
+            ),
+            ),
+            new Expanded(child:
+            new Column(
+              children: <Widget>[
+                new Icon(Icons.router,
+                  color: new Color(0xFF0000FF),
+                  ),
+                new Text("Tile 2",
+                  style: Theme.of(context).primaryTextTheme.title,
+                  ),
+              ],
+            ),
+            ),
+            new Expanded(child:
+            new Column(
+              children: <Widget>[
+                new Icon(Icons.share,
+                  color: new Color(0xFF0000FF),
+                  ),
+                new Text("Tile 3",
+                  style: Theme.of(context).primaryTextTheme.title,
+                  ),
+              ],
+            ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        new Text("""Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
+        aliquyam erat, sed diam voluptua. At vero eos et accusam et justo 
+        duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata 
+        sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, 
+        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt 
+        ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero 
+        eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 
+        gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.""",
+        style: new TextStyle(color: new Color(0xFFF0F0F0), fontFamily: 'arial',fontSize: 12.0, decoration: new TextDecoration.combine(0x0)),
+        ),
+      ],
+      
     );
   }
 }
