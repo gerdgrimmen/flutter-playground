fa473d67e932fb78400d9f36d365a7006f8ae928
procs <procs@main.lan>
2017-10-14 22:50:20 +0200

    added gesture functionality

--- material_design/lib/main.dart
+++ material_design/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(new MaterialApp(
     title: 'Flutter Tutorial',
-    home: new TutorialHome(),
+    home: new MyButton(),
   ));
 }
 
@@ -14,14 +14,14 @@ class TutorialHome extends StatelessWidget {
     return new Scaffold(
       appBar: new AppBar(
         leading: new IconButton(
-          icon: new Icon(Icons.menu),
+          icon: new Icon(Icons.menu), // top left
           tooltip: 'Navigation menu',
           onPressed: null,
         ),
         title: new Text('Example title'),
         actions: <Widget>[
           new IconButton(
-            icon: new Icon(Icons.search),
+            icon: new Icon(Icons.search), // top right
             tooltip: 'Search',
             onPressed: null,
           ),
@@ -39,3 +39,27 @@ class TutorialHome extends StatelessWidget {
     );
   }
 }
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: () {
+        print('MyButton was tapped!');
+      },
+      child: new Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: new BoxDecoration(
+          borderRadius: new BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: new Center(
+          child: new Text('Engage'),
+        ),
+      ),
+    );
+  }
+}
+
